193ebfe5619478494195db8261fcde71403eae0f
manauha <manauha@gmail.com>
2024-01-23 08:58:48 +0000

    project 2

---
+++ src/main/java/com/gorest/constants/EndPoints.java
@@ -0,0 +1,13 @@
+package com.gorest.constants;
+
+public class EndPoints {
+
+    /**
+     * This is Endpoints of User api
+     */
+    public static final String GET_SINGLE_USER_BY_ID = "/{userID}";
+    public static final String UPDATE_USER_BY_ID = "/{userID}";
+    public static final String DELETE_USER_BY_ID = "/{userID}";
+
+
+}
